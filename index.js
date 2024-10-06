1e537d4293e02fcf9dda9c49ef981446b5240ec4
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2024-10-06 13:01:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var obj = {
+  "first-name": "Sanjay",
+  "last-name": "Saravanan",
+  age: 40,
+};
+
+// Print the first name
+console.log(obj["first-name"]);
+
+// Print Age
+console.log(obj.age);
